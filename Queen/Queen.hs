bffe28d2c6be23bcf1dde8fd489feef3bc626b01
lvwenlong <lvwenlong_lambda@qq.com>
2015-10-26 00:06:30 +0800

    Simplified version

--- Queen/Queen.hs
+++ Queen/Queen.hs
@@ -2,10 +2,10 @@ module Queen where
 
 import CLaSH.Prelude
 
-type MaxSize = 5
-boardSize    = 5 :: IntData
-type IntData = (Signed 8)
-type QNbr     = Signed 8
+type MaxSize = 4
+boardSize    = 4 :: IntData
+type IntData = (Signed 4)
+type QNbr     = Signed 4
 type QVec a   = Vec MaxSize a
 type StackElm = ( QVec QNbr
                 , QNbr
@@ -77,60 +77,84 @@ queensM stack     _ = (stack', out)
       | n == (boardSize-2) && m' == 1 = Out (Just $ (qs' <~~ (n', ps' !! 0))) False
       | otherwise                     = Out Nothing False
 
-
-
-data RecvState  = RS {
-    isFinish :: Bool                -- whether finished
-    , freqDivid :: (Unsigned 16)    -- factor to divide clock
-    , solNum :: IntData             -- how many solutions are there
-    , dispCounter :: IntData        -- which solution to display
-    , solMem :: Vec 20 (QVec QNbr)  -- mem to store all solutions, 
-} deriving(Eq, Show)
-type RecvOut = (Bool, (QVec QNbr), IntData)
-
-defRecvOut = (False, def, 0)
-defFreqDiv = 999
-
-
-recvStateMachine :: RecvState -> Out -> (RecvState, RecvOut)
-recvStateMachine rs@(RS False fd sn dp sm) rIn@(Out Nothing  False) = (rs,  defRecvOut) -- calc not finished, no new solution
-recvStateMachine rs@(RS False fd sn dp sm) rIn@(Out (Just s) False) = (rs', defRecvOut) -- calc not finished, find new solution store solutions, add solution counter
-  where rs' = RS False fd (sn+1) 0 (replace sn s sm)
-recvStateMachine rs@(RS False _  sn _ sm) rIn@(Out _ True) = (rs', out) -- go into display circle
-  where rs' = RS True defFreqDiv sn 0 sm
-        out = (True, (sm !! 0), sn)
-recvStateMachine rs@(RS True 0  sn dp sm) _ = (rs', out) -- display new solution
-    where rs'   = RS True defFreqDiv sn newDp sm
-          newDp = if (dp == (sn-1)) then 0 else (dp + 1)
-          out   = (True, (sm !! dp), sn)
-recvStateMachine rs@(RS True fd sn dp sm) _ = (rs', out) 
-    where rs' = RS True (fd-1) sn dp sm
-          out = (True, (sm !! dp), sn)
-
-instance Default RecvState where
-    def = RS False defFreqDiv 0 0 def
-
-receiver = unbundle . (recvStateMachine `mealy` def)
-
-
 initStack = (0, repeat (def,0,(iterateI (+1) 1),boardSize,0)) :: Stack
 
-{-# ANN topEntity
-  (defTop
-     { t_name = "Queen"
-     , t_outputs = [ "finished"
 
-                   , "digit1"
-                   , "digit2"
-                   , "digit3"
-                   , "digit4"
-                   , "digit5"
+topEntity = fmap trans $ fetchOut $ queens $ signal Run
+    where trans :: QVec QNbr -> QVec (Unsigned 8)
+          trans = map (toEnum . fromEnum)
+queens    = queensM `mealy` initStack
 
-                   , "solution_number"] }) #-}
+fetchOut :: Signal Out -> Signal (QVec QNbr)
+fetchOut = fetchSM `mealy` def
 
-topEntity = receiver solutions 
 
-solutions = (queensM `mealy` initStack) (signal Run) :: Signal Out
+-- data Out = Out {
+--     solution :: Maybe (QVec QNbr)
+--     , finish :: Bool
+-- } deriving(Eq, Show)
+
+fetchSM :: QVec QNbr -> Out -> (QVec QNbr, QVec QNbr)
+fetchSM state (Out _ True)         = (state, state)
+fetchSM state (Out Nothing  False) = (state, state)
+fetchSM state (Out (Just v) False) = (v,v)
+-- data RecvState  = RS {
+--     isFinish :: Bool                -- whether finished
+--     , freqDivid :: (Unsigned 10)    -- factor to divide clock
+--     , solNum :: IntData             -- how many solutions are there
+--     , dispCounter :: IntData        -- which solution to display
+--     , solMem :: Vec 10 (QVec QNbr)  -- mem to store all solutions, 
+-- } deriving(Eq, Show)
+-- type RecvOut = (Bool, (QVec QNbr), IntData)
+
+-- defRecvOut = (False, def, 0)
+-- defFreqDiv = 999
+
+
+-- recvStateMachine :: RecvState -> Out -> (RecvState, RecvOut)
+-- recvStateMachine rs@(RS False fd sn dp sm) rIn@(Out Nothing  False) = (rs,  defRecvOut) -- calc not finished, no new solution
+-- recvStateMachine rs@(RS False fd sn dp sm) rIn@(Out (Just s) False) = (rs', defRecvOut) -- calc not finished, find new solution store solutions, add solution counter
+--   where rs' = RS False fd (sn+1) 0 (replace sn s sm)
+-- recvStateMachine rs@(RS False _  sn _ sm) rIn@(Out _ True) = (rs', out) -- go into display circle
+--   where rs' = RS True defFreqDiv sn 0 sm
+--         out = (True, (sm !! 0), sn)
+-- recvStateMachine rs@(RS True 0  sn dp sm) _ = (rs', out) -- display new solution
+--     where rs'   = RS True defFreqDiv sn newDp sm
+--           newDp = if (dp == (sn-1)) then 0 else (dp + 1)
+--           out   = (True, (sm !! dp), sn)
+-- recvStateMachine rs@(RS True fd sn dp sm) _ = (rs', out) 
+--     where rs' = RS True (fd-1) sn dp sm
+--           out = (True, (sm !! dp), sn)
+
+-- instance Default RecvState where
+--     def = RS False defFreqDiv 0 0 def
+
+-- receiver = unbundle . (recvStateMachine `mealy` def)
+
+
+
+
+-- {-# ANN topEntity
+--   (defTop
+--      { t_name = "Queen"
+--      , t_outputs = [ "finished"
+
+--                    , "digit1"
+--                    , "digit2"
+--                    , "digit3"
+--                    , "digit4"
+--                    , "digit5"
+
+--                    , "solution_number"] }) #-}
+
+-- topEntity = fmap2nd changeWidth $ receiver solutions 
+
+-- solutions = (queensM `mealy` initStack) (signal Run) :: Signal Out
+
+-- fmap2nd f (a, b, c) = (a, (fmap f b), c)
+
+-- changeWidth :: QVec QNbr -> QVec (Unsigned 8)
+-- changeWidth = map (toEnum . fromEnum)
 
 -- f0 (a,b,c) = a
 -- f1 (a,b,c) = b
