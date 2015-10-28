b182106ff2d32cee809af39012e37779be1af85b
alaya <lvwenlong_lambda@qq.com>
2015-10-28 14:02:36 +0800

    Add function qmap, qfoldl to QVec library

--- Queen/QVec.hs
+++ Queen/QVec.hs
@@ -37,3 +37,10 @@ hwFilterL :: (Default a) => (a->Bool)->QVec a-> QVec a
 hwFilterL pred qv@(QV list len) = 
     let filtered = imap (\i e -> (len > fromIntegral i) && pred e) list
      in foldl (\qs (e,b) -> if b then qs <~~ e else qs) def $ zip list filtered
+
+qmap :: (a->b) -> QVec a -> QVec b
+qmap f (QV v l) = QV (map f v) l
+
+qfoldl :: (a->b->a)->a->(QVec b)-> a
+qfoldl f x qv@(QV vec sz) = ifoldl newf x vec
+    where newf curr idx newv = if (fromIntegral idx) < sz then (f curr newv) else curr
