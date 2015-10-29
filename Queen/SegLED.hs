a224be4d22fa3462dbea16e8474316ef2285bcb7
alaya <lvwenlong_lambda@qq.com>
2015-10-29 12:40:49 +0800

    Add function segDecoder for debugging/testing

--- Queen/SegLED.hs
+++ Queen/SegLED.hs
@@ -18,3 +18,18 @@ segEncoder n = case n of
                  _ -> 0b00111111
 
 segV = map segEncoder
+
+-- This function should only be used for debugging and testing
+segDecoder :: (Integral a) => SegDisp -> a
+segDecoder n = case n of
+                0b00111111 -> 0
+                0b00000110 -> 1
+                0b01011011 -> 2
+                0b01001111 -> 3
+                0b01100110 -> 4
+                0b01101101 -> 5
+                0b01111101 -> 6
+                0b00000111 -> 7
+                0b01111111 -> 8
+                0b01101111 -> 9
+                _          -> undefined
